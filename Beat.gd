1309197ffc695784f3f5896be1fff15fa04fb61e
rezgi <contact@rezgi.com>
2019-11-02 13:20:22 +0000

    solved latency bug by using 1 timer instead of 3, added debug sound toggle in Beat scene.

--- Beat.gd
+++ Beat.gd
@@ -4,67 +4,39 @@ extends Node2D
 Metronome scene that takes a tempo (bpm) in the inspector and emits a signal with a dictionary 
 counting the full measure, the quarter notes and the eight notes.
 
-- optional label beats visualisation
-- bug with eight notes that get delayed over 150bpm
+Sound can be activated for debugging in inspector.
 """
 
+signal tempo_on
+
 export var bpm := 120
-signal tempo_beat
-var count_eight := 1
-var tempo = {"full" : 0, "quart" : 0, "eight" : 0.5}
+export var sound_debug := false
+
+var count_eight := 0
+var tempo = {"full" : 0, "quart" : 1, "eight" : 1}
 
 func _ready():	
-	$fullTimer.wait_time = bpm2sec(bpm / 4.0)
-	$quartTimer.wait_time = bpm2sec(bpm)
-	$eightTimer.wait_time = bpm2sec(bpm * 2.0)
-	
-	$fullTimer.start()
-	$quartTimer.start()
-	$eightTimer.start()
+	$mainTempo.wait_time = bpm2sec(bpm * 2.0)
+	$mainTempo.start()
 
 
 func bpm2sec(beat) -> float:
 	return 60.0 / beat
-#	var result = 60.0 / beat
-#	print(result)
-#	var test = stepify(60.0 / beat, 0.01)
-#	print(test)
-#	return test
 
 
-func _on_fullTimer_timeout() -> void:
-	$metronome03.play()
-	tempo.full += 1
-	tempo.quart = 0
-	tempo.eight = 0.5
-	$fullTimer.start()
-	$labelFull.text = str(tempo.full)
-
-
-func _on_quartTimer_timeout()  -> void:
-	$metronome01.play()
-	$quartTimer.start()
-	tempo.quart += 1
-	$labelQuart.text = str(tempo.quart)
-
-
-func _on_eightTimer_timeout()  -> void:
-	if count_eight % 2 == 1:
-		$metronome02.play()
-		tempo.eight += 1
+func _on_mainTempo_timeout():
+	if count_eight % 8 == 0:
+		tempo.full += 1
+		tempo.quart = 1
+		tempo.eight = 1
+		$metronome03.play() if sound_debug == true else ""
+	elif count_eight % 2 == 0:
+		tempo.quart += 1
+		$metronome01.play() if sound_debug == true else ""
+	else:
+		$metronome02.play() if sound_debug == true else ""
 	
+	emit_signal("tempo_on", tempo)
+	tempo.eight += .5
 	count_eight += 1
-	emit_signal("tempo_beat", tempo)
-	$eightTimer.start()
-	
-	$labelEight.text = str(tempo.eight)
-
-
-"""
-func round_to(value, decimal_places):
-    value factor = pow(10, decimal_places)
-    return round(value * factor) / factor
- 
-func round_to_dec(num, digit):
-    return round(num * pow(10.0, digit)) / pow(10.0, digit)
-"""
+	$mainTempo.start()
--- README.md
+++ README.md
@@ -3,10 +3,10 @@ A first try in making a platform game in Godot
 
 ## ToDo
 
-1. Make metronome * done but with a bug
-2. Metronome sends tempo data out * done, congrats
-3. player receives tempo data
-4. player moves sync to tempo
+1. Make metronome * done, and solved latency bug by using 1 timer instead of 3
+2. Metronome sends tempo data out * done
+3. player receives tempo data * WIP
+4. player moves sync to tempo * WIP
 5. player smooth movement (lerp)
 6. make player input (keyboard UP/LEFT/RIGHT)
 7. check player movement with tempo
