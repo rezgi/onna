bc0fc18ce01a4b6a1280e67ba1a2636b3ff96a6d
rezgi <contact@rezgi.com>
2019-11-04 16:23:37 +0000

    solved the movement bug thanks to help. position not yet exact.

--- Player.gd
+++ Player.gd
@@ -1,36 +1,33 @@
 extends KinematicBody2D
 
 export var speed := 1000
-export var target = Vector2.ZERO
 export var distance := 64
 
+var target = Vector2.ZERO
 var move_vector := Vector2.ZERO
 var store_tempo := 0
 var has_moved := false
-var initial_position := Vector2.ZERO
 
 func _ready():
-	var screen_size = get_viewport_rect().size
-	initial_position = screen_size / 2
-	position = initial_position
+	position = get_viewport_rect().size / 2
+	print(position)
 
 
 func _physics_process(delta):
 	if target != Vector2.ZERO:
-		move_vector.y = target.y - position.y
-		print("before : ", move_vector)
-		move_vector = move_vector.normalized() * speed
-		print("after : ", move_vector)
+		move_vector.y = ((target.y - position.y) * 0.2) / delta
+#		move_vector.y = target.y - position.y
+#		move_vector = move_vector.normalized() * speed
+		print(move_vector)
 		move_and_slide(move_vector)
 
 func test_signal(tempo):
 	if store_tempo != tempo.full:
+		has_moved = !has_moved
 		store_tempo = tempo.full
 		if !has_moved:
-			target = Vector2(0, initial_position.y - distance)
-			has_moved = true
+			target = Vector2(0, position.y - distance)
 		else:
-			target = Vector2(0, initial_position.y + distance)
-			has_moved = false
+			target = Vector2(0, position.y + distance)
 	else:
 		target = Vector2.ZERO
