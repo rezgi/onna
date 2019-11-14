988df3bb8e2c7b388e1e6b4ae4716e87d3723260
rezgi <contact@rezgi.com>
2019-11-14 15:33:47 +0000

    Better grid behavior and customization
    - Subdivisions without bugs
    - Debugged overflowing lines
    - Added in Inspector :
    -- subdivisions steps
    -- subdivisions thickness
    -- subdivisions color

--- Grid.gd
+++ Grid.gd
@@ -6,9 +6,12 @@ Debug grid with cell_size, width, color and sub-divisions options.
 """
 
 export (int) var cell_size = 32 setget _set_cell_size
+export (int) var line_thickness = 1.0 setget _set_line_thickness
 export (Color) var line_color setget _set_color
-export (int) var line_width = 1.0 setget _set_line_width
 export (bool) var cell_subdivision = false setget _set_cell_subdivision
+export var subdivision_steps := 4 setget _set_subdivision_steps
+export (int) var subdivision_thickness = 2.0 setget _set_subdivision_thickness
+export (Color) var subdivision_color setget _set_subdivision_color
 
 var screen_size := Vector2.ZERO
 
@@ -16,16 +19,17 @@ func _ready():
 	screen_size = get_viewport_rect().size
 
 func _draw():
-	for i in range(screen_size.y / cell_size):
-		draw_line(Vector2(0, i * cell_size), Vector2(screen_size.x, i * cell_size), line_color, line_width)
-	for i in range((screen_size.x + cell_size) / cell_size):
-		draw_line(Vector2(i * cell_size, 0), Vector2(i * cell_size, screen_size.y), line_color, line_width)
+	for i in range(ceil(screen_size.y / cell_size)):		# Draw vertical lines
+		draw_line(Vector2(0, i * cell_size), Vector2(screen_size.x, i * cell_size), line_color, line_thickness)
+	for i in range(ceil(screen_size.x / cell_size)):		# Draw horizontal lines
+		draw_line(Vector2(i * cell_size, 0), Vector2(i * cell_size, screen_size.y), line_color, line_thickness)
 	
 	if cell_subdivision:
-		for i in range(screen_size.y / (cell_size / 2)):
-			draw_line(Vector2(0, i * cell_size), Vector2(screen_size.x, i * cell_size), line_color, line_width * 2)
-		for i in range((screen_size.x + cell_size) / (cell_size / 2)):
-			draw_line(Vector2(i * cell_size, 0), Vector2(i * cell_size, screen_size.y), line_color, line_width * 2)
+		var new_cell_size = cell_size * subdivision_steps
+		for i in range(ceil(screen_size.y / (cell_size * subdivision_steps))):		# Draw vertical subdivision lines
+			draw_line(Vector2(0, i * new_cell_size), Vector2(screen_size.x, i * new_cell_size), subdivision_color, subdivision_thickness)
+		for i in range(ceil(screen_size.x / (cell_size * subdivision_steps))):		# Draw horizontal subdivision lines
+			draw_line(Vector2(i * new_cell_size, 0), Vector2(i * new_cell_size, screen_size.y), subdivision_color, subdivision_thickness)
 
 func _set_cell_size(value):
 	cell_size = value
@@ -35,10 +39,22 @@ func _set_color(value):
 	line_color = value
 	update()
 	
-func _set_line_width(value):
-	line_width = value
+func _set_line_thickness(value):
+	line_thickness = value
 	update()
 
 func _set_cell_subdivision(value):
 	cell_subdivision = value
+	update()
+
+func _set_subdivision_thickness(value):
+	subdivision_thickness = value
+	update()
+
+func _set_subdivision_color(value):
+	subdivision_color = value
+	update()
+
+func _set_subdivision_steps(value):
+	subdivision_steps = value
 	update()
